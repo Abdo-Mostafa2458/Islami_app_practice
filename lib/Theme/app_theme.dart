95360029ffddc23b011dba0241d9c63b0bfe221f
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2026-03-17 06:09:22 +0200

    ++finished Theme app light ++add quran_tab

---
+++ lib/Theme/app_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:new_project/Theme/colors/app_colors.dart';
+
+class AppTheme {
+  static final ThemeData appThemeLight = ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      centerTitle: true,
+      elevation: 0,
+    ),
+    scaffoldBackgroundColor: Colors.transparent,
+    textTheme: TextTheme(
+        titleMedium: TextStyle(
+            color: AppColorsLight.blackColor,
+            fontSize: 30,
+            fontWeight: FontWeight.bold),
+        bodyMedium: TextStyle(
+            color: AppColorsLight.blackColor,
+            fontWeight: FontWeight.w600,
+            fontSize: 25)),
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: AppColorsLight.primaryColor,
+        showUnselectedLabels: true,
+        unselectedIconTheme:
+            IconThemeData(color: AppColorsLight.whiteColor, size: 40),
+        // unselectedLabelStyle: TextStyle(
+        //  color: Colors.white
+        // ),
+        selectedIconTheme:
+            IconThemeData(color: AppColorsLight.blackColor, size: 50),
+        unselectedItemColor: AppColorsLight.whiteColor,
+        selectedItemColor: AppColorsLight.blackColor),
+  );
+}
---
+++ lib/Theme/colors/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColorsLight {
+  static const Color primaryColor = Color(0xFFB7935F);
+  static const Color blackColor = Color(0xFF242424);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
+
+class AppColorsDark {
+  static const Color primaryGoldenColor = Color(0xFFFACC1D);
+  static const Color blackColor = Color(0xFF242424);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
