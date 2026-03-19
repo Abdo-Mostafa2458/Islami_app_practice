78bed00dbc3ddb3c32f180fac3a8b4feb18cff4e
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2026-03-19 22:20:33 +0200

    ++finised radio_tab.dart desgin without API integration

--- lib/design_app/body_widgets/radio_tab.dart
+++ lib/design_app/body_widgets/radio_tab.dart
@@ -1,12 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:new_project/Theme/colors/app_colors.dart';
 
 class RadioTab extends StatelessWidget {
   const RadioTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,
+    return Column(
+      children: [
+        Spacer(
+          flex: 2,
+        ),
+        Image.asset(
+          "assets/images/radio_vector.png",
+          fit: BoxFit.cover,
+        ),
+        Spacer(
+          flex: 1,
+        ),
+        Text(
+          "أذاعة القرأن الكريم",
+          style: Theme.of(context)
+              .textTheme
+              .bodyMedium
+              ?.copyWith(fontWeight: FontWeight.w900),
+        ),
+        Spacer(
+          flex: 1,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.skip_previous,
+                color: AppColorsLight.primaryColor,
+                size: 40,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.arrow_right_rounded,
+                color: AppColorsLight.primaryColor,
+                size: 100,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.skip_next,
+                color: AppColorsLight.primaryColor,
+                size: 40,
+              ),
+            ),
+          ],
+        ),
+        Spacer(
+          flex: 2,
+        ),
+      ],
     );
   }
 }
