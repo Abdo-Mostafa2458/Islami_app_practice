413c9b6ea3d63ce832ea9719ca25a81a9429ed7f
abdomostafa <https://github.com/Abdo-Mostafa2458/flutter_quran_app.git>
2026-03-19 06:19:16 +0200

    ++finish quran_tab.dart ++finish quran_details.dart

--- lib/AppRoutes/AppRoutes.dart
+++ lib/AppRoutes/AppRoutes.dart
@@ -1,3 +1,4 @@
 class AppRoutes {
   static const String homeRoute = '/home';
+  static const String quranDetails = '/quranDetails';
 }
--- lib/design_app/body_widgets/quran_tab.dart
+++ lib/design_app/body_widgets/quran_tab.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:new_project/design_app/custom_quran_body.dart';
-import 'package:new_project/design_app/custom_quran_title.dart';
-import 'package:new_project/design_app/quran_hader_image.dart';
+import 'package:flutter/services.dart';
+import 'package:new_project/AppRoutes/AppRoutes.dart';
+import 'package:new_project/design_app/Widgets_Quran_tab/custom_quran_body.dart';
+import 'package:new_project/design_app/Widgets_Quran_tab/custom_quran_title.dart';
+import 'package:new_project/design_app/Widgets_Quran_tab/quran_hader_image.dart';
 
-class QuranTab extends StatelessWidget {
-  const QuranTab({super.key});
+class QuranTab extends StatefulWidget {
+  @override
+  State<QuranTab> createState() => _QuranTabState();
+}
+
+class _QuranTabState extends State<QuranTab> {
+  List<String> Sura = [];
+  List<String> verses = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +26,25 @@ class QuranTab extends StatelessWidget {
       children: [
         QuranHaderImage(),
         CustomQuranTitle(verse: "verse", surah: "surah"),
-        CustomQuranBody(),
-        CustomQuranBody(),
-        CustomQuranBody(),
-        CustomQuranBody(),
-        CustomQuranBody(),
-        CustomQuranBody(),
-        CustomQuranBody(),
-        CustomQuranBody(),
-        CustomQuranBody(),
-        CustomQuranBody(),
-        CustomQuranBody(),
+        Expanded(
+          child: Sura.isEmpty
+              ? CircularProgressIndicator()
+              : ListView.builder(
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                        onTap: () {
+                          print(index);
+
+                          Navigator.pushNamed(context, AppRoutes.quranDetails,
+                              arguments: QuranDetailsBottom(
+                                  suraName: Sura[index], indexSura: index));
+                        },
+                        child: CustomQuranBody(
+                            verse: verses[index], surah: Sura[index]));
+                  },
+                  itemCount: Sura.length,
+                ),
+        )
 
         // Container(
         //   height: 50,
@@ -33,4 +55,23 @@ class QuranTab extends StatelessWidget {
       ],
     );
   }
+
+  Future<void> loadData() async {
+    String dataSura =
+        await rootBundle.loadString("assets/files/qura_sura_name.txt");
+    String dataVerses =
+        await rootBundle.loadString("assets/files/verses_number.txt");
+    verses = dataVerses.trim().split("\n");
+    Sura = dataSura.trim().split("\n");
+    setState(() {});
+    // verses.add(dataVerses);
+    // Sura.add(dataSura);
+  }
+}
+
+class QuranDetailsBottom {
+  String suraName;
+  int indexSura;
+
+  QuranDetailsBottom({required this.suraName, required this.indexSura});
 }
